e6e9ff831e045b2a991fcb04cb2258ff1ae13c15
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-06-02 20:06:45 +0100

    HackerRank Challenges: gradingStudents

---
+++ src/hackerRank/Easy/GradingStudents/Solution.java
@@ -0,0 +1,34 @@
+package hackerRank.Easy.GradingStudents;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        // Write your code here
+
+        List<Integer> res = new ArrayList<>();
+
+        for (Integer grade : grades) {
+            int value = grade % 5;
+
+            if ((40 - grade)>= 3 ) {
+                res.add(grade);
+                continue;
+            }
+
+            if ((5 - value) >= 3){
+                res.add(grade);
+                continue;
+            }
+
+            if ((5-value) < 3){
+                res.add(grade +(5 - value));
+                continue;
+            }
+        }
+
+        return res;
+
+    }
+}

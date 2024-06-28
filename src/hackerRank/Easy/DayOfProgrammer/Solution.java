58b50ccdc8230b49634282f08c5c833d6231fd3e
Wow_RoyLuo <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-06-28 21:52:49 +0100

    HackerRank angryProfessor

---
+++ src/hackerRank/Easy/DayOfProgrammer/Solution.java
@@ -0,0 +1,45 @@
+package hackerRank.Easy.DayOfProgrammer;
+
+public class Solution {
+    public static String dayOfProgrammer(int year) {
+        // Write your code here
+        boolean isLeapYear = isLeapYear(year);
+        int noOfDaysInFeb = isLeapYear? 29 : 28;
+
+        if (year == 1918){
+            noOfDaysInFeb -= 13;
+        }
+
+        int[] daysOfMonthArr = new int[]{31, noOfDaysInFeb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int dayOfProgrammer = 256;
+
+        int day = 0;
+        int month = 0;
+        for (int i = 0; i < daysOfMonthArr.length; i++) {
+            if (dayOfProgrammer > daysOfMonthArr[i]){
+                dayOfProgrammer -= daysOfMonthArr[i];
+            } else {
+                month = i + 1;
+                day = dayOfProgrammer;
+                break;
+            }
+        }
+
+        String dayStr = day <10 ? "0" + day : Integer.toString(day);
+        String monthStr = month <10 ? "0" + month : Integer.toString(month);
+
+
+        return dayStr +"."+monthStr +"."+year;
+
+
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year <= 1918) {
+            return year % 4 == 0;
+        } else {
+            return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+        }
+    }
+}

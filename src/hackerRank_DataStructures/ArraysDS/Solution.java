dedd0b118784923316e58514e0142d9005ac5613
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-07-18 14:26:33 +0100

    HackerRank Challenges:

---
+++ src/hackerRank_DataStructures/ArraysDS/Solution.java
@@ -0,0 +1,12 @@
+package hackerRank_DataStructures.ArraysDS;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public static List<Integer> reverseArray(List<Integer> a) {
+        // Write your code here
+        Collections.reverse(a);
+        return a;
+    }
+}

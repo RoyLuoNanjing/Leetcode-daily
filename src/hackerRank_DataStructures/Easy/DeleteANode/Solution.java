d5f0a9a35837d182542c6ede890bbcb16050b469
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-07-19 13:48:18 +0100

    HackerRank Challenges

---
+++ src/hackerRank_DataStructures/Easy/DeleteANode/Solution.java
@@ -0,0 +1,32 @@
+package hackerRank_DataStructures.Easy.DeleteANode;
+
+public class Solution {
+    class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    public static SinglyLinkedListNode deleteNode(SinglyLinkedListNode llist, int position) {
+        // Write your code here
+        int count = 0;
+
+        SinglyLinkedListNode ref = llist;
+
+        if (position == 0) {
+            return ref.next;
+        }
+
+        while (count + 1 != position) {
+            llist = llist.next;
+            count++;
+        }
+        llist.next = llist.next.next;
+
+        return ref;
+    }
+}

cdb0faa84cb9849781bae0bef092dfe369801c0f
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-12-18 06:39:05 +0000

    leetcode 509

---
+++ src/leetcode/easy/Q509_FibonacciNumber/Solution_DynamicProgramming.java
@@ -0,0 +1,16 @@
+package leetcode.easy.Q509_FibonacciNumber;
+
+public class Solution_DynamicProgramming {
+    public int fib(int n) {
+        if (n < 2) return n;
+
+        int p = 0, q = 0, r = 1;
+
+        for (int i = 2; i <= n; i++) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+}
